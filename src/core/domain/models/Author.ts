9864c996db17c11a41d6bd10963b620a666075aa
Lessa Apolinario <lessaapolinario@gmail.com>
2023-09-01 20:01:10 -0300

    Add fromJSON and toJSON for Author model

--- src/core/domain/models/Author.ts
+++ src/core/domain/models/Author.ts
@@ -1,10 +1,11 @@
 import { uuid } from 'uuidv4';
+import { DTO } from '../types/DTO';
 
 class Author {
   private _id?: string;
   private _name?: string;
-  private _createdAt?: Date;
-  private _updatedAt?: Date;
+  private _created_at?: Date;
+  private _updated_at?: Date;
 
   constructor() {
     if (!this._id) {
@@ -12,8 +13,26 @@ class Author {
     }
 
     this._name = '';
-    this._createdAt = undefined;
-    this._updatedAt = undefined;
+    this._created_at = undefined;
+    this._updated_at = undefined;
+  }
+
+  static fromJSON(json: DTO): Author {
+    const author = new Author();
+    author._id = String(json['id']);
+    author._name = String(json['name']);
+    author._created_at = new Date(json['created_at'] as string);
+    author._updated_at = new Date(json['updated_at'] as string);
+    return author;
+  }
+
+  toJSON(): DTO {
+    const dto: DTO = {};
+    dto['id'] = this._id;
+    dto['name'] = this._name;
+    dto['created_at'] = this._created_at;
+    dto['updated_at'] = this._updated_at;
+    return dto;
   }
 
   get id() {
@@ -25,11 +44,11 @@ class Author {
   }
 
   get createdAt() {
-    return this._createdAt;
+    return this._created_at;
   }
 
   get updatedAt() {
-    return this._updatedAt;
+    return this._updated_at;
   }
 }
 
